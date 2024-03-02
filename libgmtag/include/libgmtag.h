9271c617ab9ecc2f4920027f09ea0a480229bec9
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-03-02 18:24:21 +0700

    header documentation

--- libgmtag/include/libgmtag.h
+++ libgmtag/include/libgmtag.h
@@ -38,8 +38,36 @@ typedef struct gmtag_def {
   GmTagTimeDef fade;
 } GmTagDef;
 
+/**
+ * @brief Generate a tags list from the contents of !tags.m3u.
+ *
+ * The tags list is private to libgmtag, and should be accessed
+ * using get_tags_for_subtune(), as that will also handle default
+ * tags, should any be defined here.
+ *
+ * When you're done with tags, you should free its memory by
+ * calling unset_tags().
+ * 
+ * @param buff !tags.m3u contents
+ */
 void tags_from_buffer (char *buff);
+
+/**
+ * @brief Get the tags for a rip's subtune.
+ *
+ * If subtune is 0 or a subtune in which tags do not exist for
+ * it, it will return the default tags (if any), defined as
+ * global tags at the beginning of the file.
+ * 
+ * @param subtune 
+ * @return GmTagDef 
+ */
 GmTagDef get_tags_for_subtune (unsigned long subtune);
+
+/**
+ * @brief Free memory associated with tags.
+ * 
+ */
 void unset_tags (void);
 
 #ifdef __cplusplus
