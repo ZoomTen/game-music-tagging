498c1d4d28911df6eb9a40798d6403cb7bab7414
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-03-13 21:05:13 +0700

    use assert aliases

--- libgmtag/src/tests/test_suite_1.c
+++ libgmtag/src/tests/test_suite_1.c
@@ -1,3 +1,4 @@
+#define MUNIT_ENABLE_ASSERT_ALIASES
 #include "munit.h"
 #include "test_suite_1.h"
 #include "libgmtag.h"
@@ -15,18 +16,18 @@ void deinit_tags (void *data) {
 
 MunitResult
 test_subtune_count (const MunitParameter params[], void *data) {
-  munit_assert_uint64(get_subtune_count(), ==, 4);
+  assert_uint64(get_subtune_count(), ==, 4);
   return MUNIT_OK;
 }
 
 MunitResult
 test_subtune_order (const MunitParameter params[], void *data) {
   GmTagOrderDef *order = get_subtune_order();
-  munit_assert_uint64(order->how_many, ==, 4);
-  munit_assert_uint64(order->order[0], ==, 1);
-  munit_assert_uint64(order->order[1], ==, 2);
-  munit_assert_uint64(order->order[2], ==, 3);
-  munit_assert_uint64(order->order[3], ==, 6);
+  assert_uint64(order->how_many, ==, 4);
+  assert_uint64(order->order[0], ==, 1);
+  assert_uint64(order->order[1], ==, 2);
+  assert_uint64(order->order[2], ==, 3);
+  assert_uint64(order->order[3], ==, 6);
   free(order->order);
   free(order);
   return MUNIT_OK;
@@ -34,27 +35,27 @@ test_subtune_order (const MunitParameter params[], void *data) {
 
 void
 check_global_tag (GmTagDef tags) {
-  munit_assert_string_equal(tags.album, "Zumi's GB Music Engine v1");
-  munit_assert_string_equal(tags.company, "Zumi");
-  munit_assert_string_equal(tags.publisher, "none");
-  munit_assert_string_equal(tags.artist, "Unknown");
-  munit_assert_uint64(tags.date.year, ==, 2015);
-  munit_assert_uint8(tags.date.month, ==, 10);
-  munit_assert_uint8(tags.date.day, ==, 9);
-  munit_assert_string_equal(tags.ripper, "Zumi");
-  munit_assert_string_equal(tags.tagger, "Zumi");
-  munit_assert_string_equal(tags.sequencer, "Zumi");
-  munit_assert_string_equal(tags.engineer, "Zumi");
+  assert_string_equal(tags.album, "Zumi's GB Music Engine v1");
+  assert_string_equal(tags.company, "Zumi");
+  assert_string_equal(tags.publisher, "none");
+  assert_string_equal(tags.artist, "Unknown");
+  assert_uint64(tags.date.year, ==, 2015);
+  assert_uint8(tags.date.month, ==, 10);
+  assert_uint8(tags.date.day, ==, 9);
+  assert_string_equal(tags.ripper, "Zumi");
+  assert_string_equal(tags.tagger, "Zumi");
+  assert_string_equal(tags.sequencer, "Zumi");
+  assert_string_equal(tags.engineer, "Zumi");
 }
 
 MunitResult
 test_subtune_1 (const MunitParameter params[], void *data) {
   GmTagDef tags = get_tags_for_subtune(1);
   check_global_tag(tags);
-  munit_assert_uint64(tags.track, ==, 1);
-  munit_assert_string_equal(tags.title, "Ocean Loader 4");
-  munit_assert_string_equal(tags.composer, "Jonathan Dunn");
-  munit_assert_string_equal(tags.comments, "Just a short snippet.");
+  assert_uint64(tags.track, ==, 1);
+  assert_string_equal(tags.title, "Ocean Loader 4");
+  assert_string_equal(tags.composer, "Jonathan Dunn");
+  assert_string_equal(tags.comments, "Just a short snippet.");
   munit_assert_type(time_t, "lu", tags.length.seconds, ==, 23);
   munit_assert_type(time_t, "lu", tags.length.miliseconds, ==, 0);
   munit_assert_type(time_t, "lu", tags.fade.seconds, ==, 5);
@@ -66,10 +67,10 @@ MunitResult
 test_subtune_2 (const MunitParameter params[], void *data) {
   GmTagDef tags = get_tags_for_subtune(2);
   check_global_tag(tags);
-  munit_assert_uint64(tags.track, ==, 12);
-  munit_assert_string_equal(tags.title, "Super Pitfall - Overworld");
-  munit_assert_ptr_equal(tags.composer, NULL);
-  munit_assert_string_equal(tags.comments, "Transcription of the NES version,\nwith extras from the PC-88 version.");
+  assert_uint64(tags.track, ==, 12);
+  assert_string_equal(tags.title, "Super Pitfall - Overworld");
+  assert_ptr_equal(tags.composer, NULL);
+  assert_string_equal(tags.comments, "Transcription of the NES version,\nwith extras from the PC-88 version.");
   munit_assert_type(time_t, "lu", tags.length.seconds, ==, 32);
   munit_assert_type(time_t, "lu", tags.length.miliseconds, ==, 0);
   munit_assert_type(time_t, "lu", tags.fade.seconds, ==, 10);
@@ -81,10 +82,10 @@ MunitResult
 test_subtune_3 (const MunitParameter params[], void *data) {
   GmTagDef tags = get_tags_for_subtune(3);
   check_global_tag(tags);
-  munit_assert_uint64(tags.track, ==, 2);
-  munit_assert_string_equal(tags.title, "Pokémon Red & Blue - Intro");
-  munit_assert_string_equal(tags.composer, "Junichi Masuda");
-  munit_assert_string_equal(tags.comments, "No noise, because I sucked");
+  assert_uint64(tags.track, ==, 2);
+  assert_string_equal(tags.title, "Pokémon Red & Blue - Intro");
+  assert_string_equal(tags.composer, "Junichi Masuda");
+  assert_string_equal(tags.comments, "No noise, because I sucked");
   munit_assert_type(time_t, "lu", tags.length.seconds, ==, 15);
   munit_assert_type(time_t, "lu", tags.length.miliseconds, ==, 0);
   munit_assert_type(time_t, "lu", tags.fade.seconds, ==, 10);
@@ -96,10 +97,10 @@ MunitResult
 test_subtune_4 (const MunitParameter params[], void *data) {
   GmTagDef tags = get_tags_for_subtune(6);
   check_global_tag(tags);
-  munit_assert_uint64(tags.track, ==, 3);
-  munit_assert_string_equal(tags.title, "We Are the Crystal Gems");
-  munit_assert_string_equal(tags.composer, "Rebecca Sugar, Aivi & Surasshu");
-  munit_assert_ptr_equal(tags.comments, NULL);
+  assert_uint64(tags.track, ==, 3);
+  assert_string_equal(tags.title, "We Are the Crystal Gems");
+  assert_string_equal(tags.composer, "Rebecca Sugar, Aivi & Surasshu");
+  assert_ptr_equal(tags.comments, NULL);
   munit_assert_type(time_t, "lu", tags.length.seconds, ==, 54);
   munit_assert_type(time_t, "lu", tags.length.miliseconds, ==, 0);
   munit_assert_type(time_t, "lu", tags.fade.seconds, ==, 10);
