deaddd8a9a1f52d3d70b21739df921dbe8ac5064
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-03-15 06:43:04 +0700

    forgot the test suite 2 header lol

---
+++ libgmtag/include/tests/test_suite_2.h
@@ -0,0 +1,8 @@
+#include "munit.h"
+
+void *init_tags_2 (const MunitParameter params[], void *data) ;
+
+void deinit_tags_2 (void *data);
+
+MunitResult
+test_migrated_tags (const MunitParameter params[], void *data) ;
