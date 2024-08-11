8c587c664e4af958e5d186e24d68840ed0f34546
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-08-11 18:48:16 +0700

    fuzzer i guess

---
+++ libgmtag/src/tests/fuzz.c
@@ -0,0 +1,46 @@
+#include "libgmtag.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (void)
+{
+  srand(time(NULL));
+
+  char something[2048] = {0};
+  something[0] = '#';
+  for (size_t i = 1; i < 2048; i++)
+  {
+    switch (rand() % 64)
+    {
+      case 0:
+      {
+        something[i] = '#';
+      }
+      break;
+      case 1:
+      {
+        something[i] = '\n';
+      }
+      break;
+      case 2:
+      {
+        something[i] = (char)(rand() % 256);
+      }
+      break;
+      default:
+      {
+        something[i] = (char)((rand() % ('Z' - '0')) + '0');
+      }
+      break;
+    }
+  }
+  something[2048 - 1] = '\0';
+
+  printf("%s", something);
+
+  tags_from_buffer(something);
+  unset_tags();
+
+  return 0;
+}
